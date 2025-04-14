7a0b71ecff55307b363a90307b712ac8fb70a84b
NasreenShaik-123 <snasreen877@gmail.com>
2025-04-14 10:47:37 +0530

    hompage is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import Registration from './Components/Registration'
+import HomePage from './pages/HomePage'
 
 function App() {
   
 
   return (
     <>
-      <Registration/>
+    <HomePage/>
+    <Registration/>
     </>
   )
 }
