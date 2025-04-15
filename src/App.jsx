a25c76b13797661c2cbe880d3b692577177a6037
Radhakrishna2526 <radhakrishnasriram020@gmail.com>
2025-04-15 10:02:42 +0530

    created Add Player Details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 
-import Registration from './Components/Registration'
-import HomePage from './pages/HomePage'
-
+import AddPlayerDetails from './pages/AddPlayerDetails'
 function App() {
-  
-
   return (
     <>
-    <HomePage/>
-    <Registration/>
+    {/* <HomePage/>
+    <Registration/> */}
+     <AddPlayerDetails />
     </>
   )
 }
 
 export default App
+
